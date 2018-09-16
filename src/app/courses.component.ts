4d62d5aa5ec782f31abf3067e946ebb7f0675617
brianokinyi <brianokinyi.bo@gmail.com>
2018-09-16 08:33:59 +0300

    Display data from  a list

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,9 +3,27 @@ import { Component } from '@angular/core';
 // Decorator function
 @Component({
     selector: 'courses', //.courses or #courses
-    template: '<h2>Courses</h2>'
+    template: `
+        <h2>{{ "Title: "+ getTitle() }}</h2>
+        <ul>
+            <li *ngFor="let course of courses">
+                {{course}}
+            </li>
+        </ul>
+        ` // Interpolation - Special syntax to render something dynamically
 })
 
 export class CoursesComponent{
 
+    title = "List of Courses";
+
+    courses = [
+        'course1',
+        'course2',
+        'course3'
+    ];
+
+    getTitle(){
+        return this.title;
+    }
 }
